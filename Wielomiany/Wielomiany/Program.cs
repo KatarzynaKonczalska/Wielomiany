7a41e3a629ee7e07d425c52c9bfd4b952d8ffb9d
Katarzyna <katarzyna.konczalska@gmail.com>
2017-12-09 16:49:53 +0100

    Init

---
+++ Wielomiany/Wielomiany/Program.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Wielomiany
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            var wielomian = new Wielomian<int>(new int[] { 1, 2, 3 });
+            var wielomian2 = new Wielomian<int>(new int[] { 1, 2, 3 });
+
+            Console.WriteLine(wielomian2.GetValue(1));
+            Console.WriteLine((wielomian+wielomian2).ToString());
+        }
+    }
+}
---
+++ Wielomiany/Wielomiany/Wielomian.cs
@@ -0,0 +1,94 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Wielomiany
+{
+    public class Wielomian<T> : ICloneable, IComparable where T : IComparable
+    {
+        private T[] wspolczynniki { get; set; }
+
+        public Wielomian(T[] wspolczynniki)
+        {
+            this.wspolczynniki = wspolczynniki;
+        }
+
+        public T GetValue(T argument)
+        {
+            T value = (dynamic) 0;
+            for(int i=0;i<this.wspolczynniki.Length;i++)
+            {
+                value += (dynamic) Math.Pow((dynamic)argument, i + 1) * this.wspolczynniki[i];
+            }
+            return value;
+        }
+
+        public static Wielomian<T> operator -(Wielomian<T> wielomianPierwszy, Wielomian<T> wielomianDrugi)
+        {
+            Wielomian<T> dluzszyWielomian, krotszyWielomian;
+            if (wielomianPierwszy.wspolczynniki.Length > wielomianDrugi.wspolczynniki.Length)
+            {
+                dluzszyWielomian = wielomianPierwszy;
+                krotszyWielomian = wielomianDrugi;
+            }
+            else
+            {
+                dluzszyWielomian = wielomianDrugi;
+                krotszyWielomian = wielomianPierwszy;
+            }
+            for (int i = 0; i < krotszyWielomian.wspolczynniki.Length; i++)
+            {
+                dluzszyWielomian.wspolczynniki[i] -= (dynamic)krotszyWielomian.wspolczynniki[i];
+            }
+            return dluzszyWielomian;
+        }
+
+        public static Wielomian<T> operator +(Wielomian<T> wielomianPierwszy, Wielomian<T> wielomianDrugi)
+        {
+            Wielomian<T> dluzszyWielomian, krotszyWielomian;
+            if (wielomianPierwszy.wspolczynniki.Length > wielomianDrugi.wspolczynniki.Length)
+            {
+                dluzszyWielomian = wielomianPierwszy;
+                krotszyWielomian = wielomianDrugi;
+            }
+            else
+            {
+                dluzszyWielomian = wielomianDrugi;
+                krotszyWielomian = wielomianPierwszy;
+            }
+            for (int i = 0; i < krotszyWielomian.wspolczynniki.Length; i++)
+            {
+                dluzszyWielomian.wspolczynniki[i] += (dynamic)krotszyWielomian.wspolczynniki[i];
+            }
+            return dluzszyWielomian;
+        }
+
+        public override string ToString()
+        {
+            StringBuilder builder = new StringBuilder();
+            for (int i = 0; i < this.wspolczynniki.Length; i++)
+            {
+                builder.Append(this.wspolczynniki[i]).Append("*x^").Append(i + 1).Append("+");
+            }
+            builder.Length--;
+            return builder.ToString();
+        }
+
+        public object Clone()
+        {
+            return new Wielomian<T>(this.wspolczynniki);
+        }
+
+        public int CompareTo(object obj)
+        {
+            if (obj == null) return 1;
+            Wielomian<T> innyWieomian = obj as Wielomian<T>;
+            if (innyWieomian != null)
+                return Convert.ToInt32(this.wspolczynniki.SequenceEqual(innyWieomian.wspolczynniki));
+            else
+                throw new ArgumentException("Object is not a Wielomian");
+        }
+    }
+}
